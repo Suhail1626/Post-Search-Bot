bb5f1a0ee034e8fa2749a69395d67f2f9c70b895
Suhail1626 <104824273+Suhail1626@users.noreply.github.com>
2024-11-03 10:33:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
+API_ID       = "24135304"
+API_HASH     = "7bdcc6969ab723de66b02e64897b7184"
 BOT_TOKEN    = ""
 SESSION      = ""
 DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "RahulReviewsYT"
+LOG_CHANNEL  = ""
+ADMIN        = ""
+CHANNEL      = ""
